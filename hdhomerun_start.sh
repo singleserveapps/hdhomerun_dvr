5c9157c3f4f92c8eab99ac1d70c201f6918fb460
arraylabs <chris@arraylabs.com>
2017-03-02 18:09:25 -0500

    Update hdhomerun_start.sh

--- hdhomerun_start.sh
+++ hdhomerun_start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [ "$SAMBA" != "false" ] && [ "$SAMBA" != "FALSE" ] && [ "$SAMBA" != "off" ] && [ "$SAMBA" != "OFF" ];then
+	echo "Samba enabled" > /tmp/samba_status.txt
+	/usr/sbin/smbd -D -d1 --option=workgroup=${workgroup:-workgroup}
+else
+	echo "Samba disabled" > /tmp/samba_status.txt
+fi
 /hdhomerun/bin/hdhomerun_record_linux foreground --conf /hdhomerun/etc/hdhomerun.conf
